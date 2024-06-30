8d85c89b46bf370c02310f0c903528306f36a34d
Domingo-Jhana Christina L.-BSIT 32E2 <161099915+Malones-MaryChel-BSIT32E2@users.noreply.github.com>
2024-06-30 22:53:46 +0800

    Add project files.

---
+++ PREFINAL_ASSIGNMENT_TWO_POKEMON_MALONES_MARYCHEL_BSIT_32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_MALONES_MARYCHEL_BSIT_32E2.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Level { get; set; }
+    }
+}
